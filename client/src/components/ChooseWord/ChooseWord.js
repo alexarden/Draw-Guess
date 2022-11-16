e635b11a0b98b0428ae7c362f06f87edb9e93f69
alex <alexarden@cyber4s.dev>
2022-11-16 20:30:59 +0200

    Get words by difficulties.

--- client/src/components/ChooseWord/ChooseWord.js
+++ client/src/components/ChooseWord/ChooseWord.js
@@ -1,12 +1,44 @@
+import randomWords from 'random-words';
+
 export default function ChooseWord(){
+
+const handleEasyClick = () => {
+     console.log('easy');
+     let easyWord = ['']; 
+     while(easyWord[0].length !== 3){
+        easyWord = randomWords({exactly:1, maxLength:3})
+     }
+     console.log(easyWord[0].length, easyWord[0]);   
+     return 
+     
+}
+const handleMedClick = () => {
+     console.log('medium');
+     let mediumWord = [''];  
+     while(mediumWord[0].length !== 4){
+        mediumWord = randomWords({exactly:1, maxLength:4})
+     }
+     console.log(mediumWord[0].length, mediumWord[0]);   
+     return 
+     
+}
+const handleHardClick = () => {
+     console.log('hard');
+     let hardWord = ['']; 
+     while(hardWord[0].length < 5){
+        hardWord = randomWords({exactly:1, maxLength:12})
+     }
+     console.log(hardWord[0].length, hardWord[0]);   
+     return 
+}
 
 return (
     <>
         <h2>Choose Difficulty</h2> 
 
-        <button>EASY</button> 
-        <button>MEDIUM</button>
-        <button>HARD</button>
+        <button onClick={handleEasyClick}>EASY</button>  
+        <button onClick={handleMedClick}>MEDIUM</button> 
+        <button onClick={handleHardClick}>HARD</button>
     </>
 )
 
