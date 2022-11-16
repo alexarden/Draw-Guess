1f99bc3adc7e3b3547d9303bfcdc40563d5f50e7
alex <alexarden@cyber4s.dev>
2022-11-16 12:20:29 +0200

    Can draw on canvas and save data.

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,43 +1,38 @@
 import { useEffect, useState, useRef } from "react";
 import CanvasDraw from 'react-canvas-draw'; 
-
-function debounce(fn, ms) {
-    let timer
-    return _ => {
-      clearTimeout(timer)
-      timer = setTimeout(_ => {
-        timer = null
-        fn.apply(this, arguments)
-      }, ms)
-    };
-  }
+import style from './Canvas.module.scss';
 
 export default function Canvas() {
 
-  const [size, setSize] = useState(0)
+  const [size, setSize] = useState(200)
 
   const canvasRef = useRef(null)
 
-  const deboundCanvasChange = function handleCanvasChange() {
+  const canvasChange = function handleCanvasChange() {
     const canvas = canvasRef.current;
     const drawings = canvas.getSaveData();
-
-
+    console.log(drawings); 
   }
 
   useEffect(() => {
     const height = window.innerHeight
     const width = window.innerWidth
-    const fixedSize = height > width ? height : width
+    const fixedSize = height > width ? height * 0.7 : width * 0.7  
     setSize(fixedSize);
   },[size])   
 
 
   return (
       <CanvasDraw
+        className={style.canvas} 
         lazyRadius={0}
         brushColor={'black'}
         brushRadius={5}
+        canvasWidth={size}
+        canvasHeight={size}  
+        ref={canvasRef}
+        disabled={false}
+        onChange={canvasChange}
       />
   );
 }
